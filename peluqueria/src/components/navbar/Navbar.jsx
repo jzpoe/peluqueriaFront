57483709ae56fa35c79e8b2c4cc423b04fce2700
jzpoe <pmguez1287@gmail.com>
2023-12-27 10:06:54 -0500

    se agrega navbar resposive

--- peluqueria/src/components/navbar/Navbar.jsx
+++ peluqueria/src/components/navbar/Navbar.jsx
@@ -1,39 +1,48 @@
-import "./navbar.css"
-import { Link, Outlet } from 'react-router-dom';
+import { useState } from "react";
+import "./navbar.css";
+import { Link, Outlet } from "react-router-dom";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  const closeMenu=() => {
+    setMenuOpen(false)
+  }
+
+
   return (
-    
-        <div className="container-navbar">
-                    
+    <div className="container-navbar">
+              <button className='menuhamburger' onClick={toggleMenu}>☰</button>
 
-        <nav className="navegacion">
-          <ul>
-          <li>
-              <Link to="/peluqueria">peluqueria</Link>
-            </li>
-            <li>
-              <Link to="/Home">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">Nosotros</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contacto</Link>
-            </li>
-            <li className="inicioSesion" >
-              <Link to="/sesion"  >inicio de sesion</Link>
-            </li>
-          </ul>
-
-        </nav>
-
-        <Outlet/>
-        </div>
+      <nav className="navegacion">
         
+      <ul className={`enlaces ${menuOpen ? "open" : ""}`}>
 
-    
-  )
-}
+          <li>
+            <Link to="/peluqueria" onClick={closeMenu}>peluqueria</Link>
+          </li>
+          <li>
+            <Link to="/Home" onClick={closeMenu}>Home</Link>
+          </li>
+          <li>
+            <Link to="/about"onClick={closeMenu}>Nosotros</Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={closeMenu}>Contacto</Link>
+          </li>
+          <li className="inicioSesion" onClick={closeMenu}>
+            <Link to="/sesion">inicio de sesion</Link>
+          </li>
+        </ul>
+      </nav>
 
-export default Navbar
+      <Outlet />
+    </div>
+  );
+};
+
+export default Navbar;
