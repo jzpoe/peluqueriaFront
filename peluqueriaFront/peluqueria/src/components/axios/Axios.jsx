1a19b7125ad8ae649d815cf6916789e9ac1296f4
unknown <pmguez1287@gmail.com>
2024-01-06 15:10:48 -0500

    configuracion de cors 8

--- peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
+++ peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
@@ -4,6 +4,5 @@ import axios from "axios";
 //axios.defaults.baseURL = "http://localhost:3001"
 
 axios.defaults.baseURL = "https://back-peluqueria.vercel.app";  // Reemplaza con la URL de tu servidor
-axios.defaults.withCredentials = true; // Habilita el intercambio de cookies o encabezados de autorización
 
 export default axios;
