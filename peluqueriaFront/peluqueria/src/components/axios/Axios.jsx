cbc85052201bd33bc8fdf815f7a4f96b03878a84
unknown <pmguez1287@gmail.com>
2024-01-06 09:59:00 -0500

    se retira backend del repositorio y se deja por separado

--- peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
+++ peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 //axios.defaults.baseURL = "http://localhost:3001"
-axios.defaults.baseURL = "https://back-peluqueria.vercel.app/"
+axios.defaults.baseURL = "back-peluqueria-aiy3vsl62-jzpoe.vercel.app"
 axios.defaults.withCredentials = true;
 
 export default axios;
