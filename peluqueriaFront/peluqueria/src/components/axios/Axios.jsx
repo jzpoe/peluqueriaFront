8d2ae770769b02086dc62f229f261b3f41927f18
unknown <pmguez1287@gmail.com>
2024-01-06 17:51:48 -0500

    configuracion de cors 10

--- peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
+++ peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
@@ -3,6 +3,6 @@ import axios from "axios";
 
 //axios.defaults.baseURL = "http://localhost:3001"
 
-axios.defaults.baseURL = "back-peluqueria-i512ws466-jzpoe.vercel.app";  // Reemplaza con la URL de tu servidor
+axios.defaults.baseURL = "back-peluqueria.vercel.app";  // Reemplaza con la URL de tu servidor
 
 export default axios;
