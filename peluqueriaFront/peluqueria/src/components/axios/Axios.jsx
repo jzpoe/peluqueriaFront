34c954d2fc4dc2c91d15e944c22a154765b06474
unknown <pmguez1287@gmail.com>
2024-01-06 12:16:58 -0500

    cambio en la configuracion de axios

--- peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
+++ peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 //axios.defaults.baseURL = "http://localhost:3001"
 //axios.defaults.baseURL = "back-peluqueria.vercel.app"
 var xhr = new XMLHttpRequest();
-xhr.open('POST', 'back-peluqueria.vercel.app/sesion', true);
+xhr.open('POST', 'back-peluqueria.vercel.app', true);
 xhr.withCredentials = true; // Habilita el intercambio de cookies o encabezados de autorización
 xhr.send();
 
