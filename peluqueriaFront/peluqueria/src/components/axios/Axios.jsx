003f3b3858b5fac65524123f56dd4a0b8f6d22c5
unknown <pmguez1287@gmail.com>
2024-01-06 11:34:45 -0500

    se ingresa nuevas carpetas

--- peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
+++ peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
@@ -2,7 +2,10 @@ import axios from "axios";
 
 
 //axios.defaults.baseURL = "http://localhost:3001"
-axios.defaults.baseURL = "back-peluqueria-aiy3vsl62-jzpoe.vercel.app"
-axios.defaults.withCredentials = true;
+//axios.defaults.baseURL = "back-peluqueria.vercel.app"
+var xhr = new XMLHttpRequest();
+xhr.open('POST', 'back-peluqueria.vercel.app/sesion', true);
+xhr.withCredentials = true; // Habilita el intercambio de cookies o encabezados de autorización
+xhr.send();
 
 export default axios;
