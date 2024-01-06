d2f467fe62b52273ec11ee423e8b38305f7271ab
unknown <pmguez1287@gmail.com>
2024-01-06 14:29:02 -0500

    configuracion de cors 6

--- peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
+++ peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 //axios.defaults.baseURL = "http://localhost:3001"
 
-
+axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "https://back-peluqueria.vercel.app";  // Reemplaza con la URL de tu servidor
 axios.defaults.withCredentials = true; // Habilita el intercambio de cookies o encabezados de autorización
 
