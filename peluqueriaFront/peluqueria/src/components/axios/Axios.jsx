3d73925f13657bebcf43047a140cb1602adc328b
unknown <pmguez1287@gmail.com>
2024-01-06 19:13:40 -0500

    configuracion de cors 14

--- peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
+++ peluqueriaFront/peluqueria/src/components/axios/Axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 //axios.defaults.baseURL = "http://localhost:3001"
 
-axios.defaults.baseURL = "https://back-peluqueria-lpdald3km-jzpoe.vercel.app/";  // Reemplaza con la URL de tu servidor
+axios.defaults.baseURL = "https://back-peluqueria.vercel.app/";  // Reemplaza con la URL de tu servidor
 axios.defaults.withCredentials = true;
 
 export default axios;
